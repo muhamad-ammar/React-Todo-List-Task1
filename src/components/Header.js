c7314289e85a621b0a038e07618212ef1f006f94
Muhammad Ammar <57048049+muhamad-ammar@users.noreply.github.com>
2022-03-12 13:27:10 +0500

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// Fat arrow function based Component
+// no render() in functional components
+const Header = () => {
+  return (
+    <header>
+      <h1>Todo List</h1>
+    </header>
+  )
+}
+// Exporting
+export default Header;
